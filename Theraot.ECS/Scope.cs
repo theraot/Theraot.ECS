8e408259b81fa89699287b5913ce29e229620ddd
Alfonso J. Ramos <theraot@gmail.com>
2019-07-25 10:06:01 -0500

    Moved Query to new partial class

--- Theraot.ECS/Scope.cs
+++ Theraot.ECS/Scope.cs
@@ -7,7 +7,7 @@ using QueryId = System.Int32;
 
 namespace Theraot.ECS
 {
-    public class Scope<TEntity>
+    public partial class Scope<TEntity>
     {
         private const int Add = 1;
         private const int Noop = 0;
@@ -54,120 +54,6 @@ namespace Theraot.ECS
                 return result;
             }
             return Array.Empty<TEntity>();
-        }
-
-        public void Query<TComponent>(QueryId queryId, Action<TEntity, TComponent> callback)
-        {
-            if (callback == null)
-            {
-                throw new ArgumentNullException(nameof(callback));
-            }
-            foreach (var entity in GetEntities(queryId))
-            {
-                callback(entity, GetComponent<TComponent>(entity));
-            }
-        }
-
-        public void Query<TComponent1, TComponent2>(QueryId queryId, Action<TEntity, TComponent1, TComponent2> callback)
-        {
-            if (callback == null)
-            {
-                throw new ArgumentNullException(nameof(callback));
-            }
-            foreach (var entity in GetEntities(queryId))
-            {
-                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity));
-            }
-        }
-
-        public void Query<TComponent1, TComponent2, TComponent3>(QueryId queryId, Action<TEntity, TComponent1, TComponent2, TComponent3> callback)
-        {
-            if (callback == null)
-            {
-                throw new ArgumentNullException(nameof(callback));
-            }
-            foreach (var entity in GetEntities(queryId))
-            {
-                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity), GetComponent<TComponent3>(entity));
-            }
-        }
-
-        public void Query<TComponent>(Action<TEntity, TComponent> callback)
-        {
-            if (callback == null)
-            {
-                throw new ArgumentNullException(nameof(callback));
-            }
-            var query = new Query
-            (
-                new HashSet<ComponentType>
-                (
-                    new[]
-                    {
-                        GetComponentType<TComponent>()
-                    }
-                ),
-                new HashSet<ComponentType>(),
-                new HashSet<ComponentType>()
-            );
-            var queryId = RegisterQuery(query);
-            foreach (var entity in GetEntities(queryId))
-            {
-                callback(entity, GetComponent<TComponent>(entity));
-            }
-        }
-
-        public void Query<TComponent1, TComponent2>(Action<TEntity, TComponent1, TComponent2> callback)
-        {
-            if (callback == null)
-            {
-                throw new ArgumentNullException(nameof(callback));
-            }
-            var query = new Query
-            (
-                new HashSet<ComponentType>
-                (
-                    new[]
-                    {
-                        GetComponentType<TComponent1>(),
-                        GetComponentType<TComponent2>()
-                    }
-                ),
-                new HashSet<ComponentType>(),
-                new HashSet<ComponentType>()
-            );
-            var queryId = RegisterQuery(query);
-            foreach (var entity in GetEntities(queryId))
-            {
-                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity));
-            }
-        }
-
-        public void Query<TComponent1, TComponent2, TComponent3>(Action<TEntity, TComponent1, TComponent2, TComponent3> callback)
-        {
-            if (callback == null)
-            {
-                throw new ArgumentNullException(nameof(callback));
-            }
-            var query = new Query
-            (
-                new HashSet<ComponentType>
-                (
-                    new[]
-                    {
-                        GetComponentType<TComponent1>(),
-                        GetComponentType<TComponent2>(),
-                        GetComponentType<TComponent3>()
-                    }
-                ),
-                new HashSet<ComponentType>(),
-                new HashSet<ComponentType>()
-            );
-            var queryId = RegisterQuery(query);
-            foreach (var entity in GetEntities(queryId))
-            {
-                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity), GetComponent<TComponent3>(entity));
-            }
         }
 
         public QueryId RegisterQuery(Query query)
---
+++ Theraot.ECS/Scope.query.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Theraot.ECS
+{
+    public partial class Scope<TEntity>
+    {
+        public void Query<TComponent>(Int32 queryId, Action<TEntity, TComponent> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            foreach (var entity in GetEntities(queryId))
+            {
+                callback(entity, GetComponent<TComponent>(entity));
+            }
+        }
+
+        public void Query<TComponent1, TComponent2>(Int32 queryId, Action<TEntity, TComponent1, TComponent2> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            foreach (var entity in GetEntities(queryId))
+            {
+                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity));
+            }
+        }
+
+        public void Query<TComponent1, TComponent2, TComponent3>(Int32 queryId, Action<TEntity, TComponent1, TComponent2, TComponent3> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            foreach (var entity in GetEntities(queryId))
+            {
+                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity), GetComponent<TComponent3>(entity));
+            }
+        }
+
+        public void Query<TComponent>(Action<TEntity, TComponent> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            var query = new Query
+            (
+                new HashSet<Type>
+                (
+                    new[]
+                    {
+                        GetComponentType<TComponent>()
+                    }
+                ),
+                new HashSet<Type>(),
+                new HashSet<Type>()
+            );
+            var queryId = RegisterQuery(query);
+            foreach (var entity in GetEntities(queryId))
+            {
+                callback(entity, GetComponent<TComponent>(entity));
+            }
+        }
+
+        public void Query<TComponent1, TComponent2>(Action<TEntity, TComponent1, TComponent2> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            var query = new Query
+            (
+                new HashSet<Type>
+                (
+                    new[]
+                    {
+                        GetComponentType<TComponent1>(),
+                        GetComponentType<TComponent2>()
+                    }
+                ),
+                new HashSet<Type>(),
+                new HashSet<Type>()
+            );
+            var queryId = RegisterQuery(query);
+            foreach (var entity in GetEntities(queryId))
+            {
+                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity));
+            }
+        }
+
+        public void Query<TComponent1, TComponent2, TComponent3>(Action<TEntity, TComponent1, TComponent2, TComponent3> callback)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            var query = new Query
+            (
+                new HashSet<Type>
+                (
+                    new[]
+                    {
+                        GetComponentType<TComponent1>(),
+                        GetComponentType<TComponent2>(),
+                        GetComponentType<TComponent3>()
+                    }
+                ),
+                new HashSet<Type>(),
+                new HashSet<Type>()
+            );
+            var queryId = RegisterQuery(query);
+            foreach (var entity in GetEntities(queryId))
+            {
+                callback(entity, GetComponent<TComponent1>(entity), GetComponent<TComponent2>(entity), GetComponent<TComponent3>(entity));
+            }
+        }
+    }
+}
