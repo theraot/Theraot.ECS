628f6382b2f089422fb3f715bcb1e153e47e67ab
Alfonso J. Ramos <theraot@gmail.com>
2019-07-27 13:21:54 -0500

    More Tests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using NUnit.Framework;
 using Theraot.ECS;
 
@@ -5,6 +6,16 @@ namespace Tests
 {
     public class Tests
     {
+        [Test]
+        public void CreateEntityAreDifferent()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
+            var entityA = scope.CreateEntity();
+            var entityB = scope.CreateEntity();
+            Assert.AreNotEqual(entityA, entityB);
+        }
+
         [Test]
         public void GuidBitArray()
         {
@@ -23,6 +34,36 @@ namespace Tests
             var obj = new object();
             scope.SetComponent(entity, obj);
             Assert.AreEqual(obj, scope.GetComponent<object>(entity));
+        }
+
+        [Test]
+        public void QueryAfterEntities()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
+            var entityA = scope.CreateEntity();
+            var entityB = scope.CreateEntity();
+            scope.SetComponent(entityA, 100);
+            var query = scope.CreateQuery(new[] { typeof(int) }, System.Type.EmptyTypes, System.Type.EmptyTypes);
+            var entities = scope.GetEntities(query).ToArray();
+            Assert.AreEqual(1, entities.Length);
+            Assert.AreEqual(entityA, entities[0]);
+            Assert.AreNotEqual(entityB, entities[0]);
+        }
+
+        [Test]
+        public void QueryBeforeEntities()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
+            var query = scope.CreateQuery(new[] { typeof(int) }, System.Type.EmptyTypes, System.Type.EmptyTypes);
+            var entityA = scope.CreateEntity();
+            var entityB = scope.CreateEntity();
+            scope.SetComponent(entityA, 100);
+            var entities = scope.GetEntities(query).ToArray();
+            Assert.AreEqual(1, entities.Length);
+            Assert.AreEqual(entityA, entities[0]);
+            Assert.AreNotEqual(entityB, entities[0]);
         }
 
         [Test]
