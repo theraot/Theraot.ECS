7265d9a0ddb8f509cc1da8b303bdff491acf5dff
Alfonso J. Ramos <theraot@gmail.com>
2019-07-27 12:51:08 -0500

    Initial tests
    Passing all

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,13 +1,50 @@
 using NUnit.Framework;
+using Theraot.ECS;
 
 namespace Tests
 {
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void RecoverValueTypeBitArray()
         {
-            Assert.Pass();
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new BitArrayStrategy(1));
+            var entity = scope.CreateEntity();
+            scope.SetComponent(entity, 42);
+            Assert.AreEqual(42, scope.GetComponent<int>(entity));
+        }
+
+        [Test]
+        public void RecoverValueTypeTypeHashSet()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
+            var entity = scope.CreateEntity();
+            scope.SetComponent(entity, 42);
+            Assert.AreEqual(42, scope.GetComponent<int>(entity));
+        }
+
+        [Test]
+        public void RecoverObjectBitArray()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new BitArrayStrategy(1));
+            var entity = scope.CreateEntity();
+            var obj = new object();
+            scope.SetComponent(entity, obj);
+            Assert.AreEqual(obj, scope.GetComponent<object>(entity));
+        }
+
+        [Test]
+        public void RecoverObjectTypeHashSet()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
+            var entity = scope.CreateEntity();
+            var obj = new object();
+            scope.SetComponent(entity, obj);
+            Assert.AreEqual(obj, scope.GetComponent<object>(entity));
         }
     }
 }
