f159f3990b4177517aefd63592b53d87025db5c7
Alfonso J. Ramos <theraot@gmail.com>
2019-07-27 10:49:27 -0500

    Removed SetComponent with four components

--- Theraot.ECS/IScope.cs
+++ Theraot.ECS/IScope.cs
@@ -10,7 +10,6 @@ namespace Theraot.ECS
         void SetComponent<TComponent>(TEntity entity, TComponent component);
         void SetComponent<TComponent1, TComponent2>(TEntity entity, TComponent1 component1, TComponent2 component2);
         void SetComponent<TComponent1, TComponent2, TComponent3>(TEntity entity, TComponent1 component1, TComponent2 component2, TComponent3 component3);
-        void SetComponent<TComponent1, TComponent2, TComponent3, TComponent4>(TEntity entity, TComponent1 component1, TComponent2 component2, TComponent3 component3, TComponent4 component4);
         void SetComponent(TEntity entity, params Component[] components);
         void Query<TComponent>(QueryId queryId, Action<TEntity, TComponent> callback);
         void Query<TComponent1, TComponent2>(QueryId queryId, Action<TEntity, TComponent1, TComponent2> callback);
--- Theraot.ECS/Scope.setComponent.cs
+++ Theraot.ECS/Scope.setComponent.cs
@@ -56,23 +56,6 @@ namespace Theraot.ECS
             UpdateEntitiesByQueryOnAddedComponents(entity, allComponentsTypes, addedComponents);
         }
 
-        public void SetComponent<TComponent1, TComponent2, TComponent3, TComponent4>(TEntity entity, TComponent1 component1, TComponent2 component2, TComponent3 component3, TComponent4 component4)
-        {
-            var allComponents = _componentsByEntity[entity];
-            var addedComponents = allComponents.SetAll
-            (
-                new Component[] {component1, component2, component3, component4},
-                new[] {GetComponentType(component1), GetComponentType(component2), GetComponentType(component3), GetComponentType(component4)}
-            ).ToArray();
-            if (addedComponents.Length == 0)
-            {
-                return;
-            }
-
-            var allComponentsTypes = _componentTypesByEntity[entity];
-            UpdateEntitiesByQueryOnAddedComponents(entity, allComponentsTypes, addedComponents);
-        }
-
         public void SetComponent(TEntity entity, params Component[] components)
         {
             var allComponents = _componentsByEntity[entity];
