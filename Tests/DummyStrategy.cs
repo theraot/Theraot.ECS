12d75510c24814cbebcd50b68839b1e2f9778e68
Alfonso J. Ramos <theraot@gmail.com>
2019-07-28 04:51:25 -0500

    Reworked Tests

---
+++ Tests/DummyStrategy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Theraot.ECS;
+
+namespace Tests
+{
+    internal class DummyStrategy : IComponentQueryStrategy<int, int, int>
+    {
+        public int CreateComponentTypeSet(Dictionary<int, object> dictionary)
+        {
+            return 0;
+        }
+
+        public int CreateQuery(IEnumerable<int> all, IEnumerable<int> any, IEnumerable<int> none)
+        {
+            return 0;
+        }
+
+        public QueryCheckResult QueryCheck(int allComponentsTypes, int query)
+        {
+            return QueryCheckResult.Noop;
+        }
+
+        public QueryCheckResult QueryCheckOnAddedComponent(int addedComponentType, int allComponentsTypes, int query)
+        {
+            return QueryCheckResult.Noop;
+        }
+
+        public QueryCheckResult QueryCheckOnAddedComponents(IEnumerable<int> addedComponentTypes, int allComponentsTypes, int query)
+        {
+            return QueryCheckResult.Noop;
+        }
+
+        public QueryCheckResult QueryCheckOnRemovedComponent(int removedComponentType, int allComponentsTypes, int query)
+        {
+            return QueryCheckResult.Noop;
+        }
+
+        public QueryCheckResult QueryCheckOnRemovedComponents(IEnumerable<int> removedComponentTypes, int allComponentsTypes, int query)
+        {
+            return QueryCheckResult.Noop;
+        }
+
+        public void SetComponentType(int componentTypeSet, int componentType)
+        {
+            
+        }
+
+        public void SetComponentTypes(int componentTypeSet, IEnumerable<int> componentTypes)
+        {
+            
+        }
+
+        public void UnsetComponentType(int componentTypeSet, int componentType)
+        {
+            
+        }
+
+        public void UnsetComponentTypes(int componentTypeSet, IEnumerable<int> componentTypes)
+        {
+            
+        }
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Linq;
 using NUnit.Framework;
 using Theraot.ECS;
@@ -10,102 +11,98 @@ namespace Tests
         public void CreateEntityAreDifferent()
         {
             var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
+            var scope = Scope.CreateScope(() => entityId++, new DummyStrategy());
             var entityA = scope.CreateEntity();
             var entityB = scope.CreateEntity();
             Assert.AreNotEqual(entityA, entityB);
         }
 
         [Test]
-        public void GuidBitArray()
+        public void Guid()
         {
-            var scope = Scope.CreateScope(System.Guid.NewGuid, new BitArrayStrategy(1));
-            var entity = scope.CreateEntity();
-            var obj = new object();
-            scope.SetComponent(entity, 0, obj);
-            Assert.AreEqual(obj, scope.GetComponent<object>(entity, 0));
-        }
-
-        [Test]
-        public void GuidTypeHashSet()
-        {
-            var scope = Scope.CreateScope(System.Guid.NewGuid, new TypeHashSetStrategy());
-            var entity = scope.CreateEntity();
-            var obj = new object();
-            scope.SetComponent(entity, "a" , obj);
-            Assert.AreEqual(obj, scope.GetComponent<object>(entity, "a"));
+            Guid(Scope.CreateScope(System.Guid.NewGuid, new BitArrayStrategy(1)), 0);
+            Guid(Scope.CreateScope(IncrementingInt(), new TypeHashSetStrategy()), "a");
         }
 
         [Test]
         public void QueryAfterEntities()
         {
-            var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
-            var entityA = scope.CreateEntity();
-            var entityB = scope.CreateEntity();
-            scope.SetComponent(entityA, "a", 100);
-            var query = scope.CreateQuery(new[] { "a" }, System.Array.Empty<string>(), System.Array.Empty<string>());
-            var entities = scope.GetEntities(query).ToArray();
-            Assert.AreEqual(1, entities.Length);
-            Assert.AreEqual(entityA, entities[0]);
-            Assert.AreNotEqual(entityB, entities[0]);
+            QueryAfterEntities(Scope.CreateScope(System.Guid.NewGuid, new BitArrayStrategy(1)), 0);
+            QueryAfterEntities(Scope.CreateScope(IncrementingInt(), new TypeHashSetStrategy()), "a");
         }
 
         [Test]
         public void QueryBeforeEntities()
         {
-            var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
-            var query = scope.CreateQuery(new[] { "a" }, System.Array.Empty<string>(), System.Array.Empty<string>());
+            QueryBeforeEntities(Scope.CreateScope(System.Guid.NewGuid, new BitArrayStrategy(1)), 0);
+            QueryBeforeEntities(Scope.CreateScope(IncrementingInt(), new TypeHashSetStrategy()), "a");
+        }
+
+        [Test]
+        public void RecoverObject()
+        {
+            RecoverObject(Scope.CreateScope(System.Guid.NewGuid, new BitArrayStrategy(1)), 0);
+            RecoverObject(Scope.CreateScope(IncrementingInt(), new TypeHashSetStrategy()), "a");
+        }
+
+        [Test]
+        public void RecoverValueType()
+        {
+            RecoverValueType(Scope.CreateScope(System.Guid.NewGuid, new BitArrayStrategy(1)), 0);
+            RecoverValueType(Scope.CreateScope(IncrementingInt(), new TypeHashSetStrategy()), "a");
+        }
+
+        private static Func<int> IncrementingInt()
+        {
+            var value = 0;
+            return () => value++;
+        }
+
+        private static void Guid<TEntity, TComponentType> (IScope<TEntity, TComponentType> scope, TComponentType type)
+        {
+            var entity = scope.CreateEntity();
+            var obj = new object();
+            scope.SetComponent(entity, type, obj);
+            Assert.AreEqual(obj, scope.GetComponent<object>(entity, type));
+        }
+
+        private static void QueryAfterEntities<TEntity, TComponentType> (IScope<TEntity, TComponentType> scope, TComponentType type)
+        {
             var entityA = scope.CreateEntity();
             var entityB = scope.CreateEntity();
-            scope.SetComponent(entityA, "a", 100);
+            scope.SetComponent(entityA, type, 100);
+            var query = scope.CreateQuery(new[] { type }, Array.Empty<TComponentType>(), Array.Empty<TComponentType>());
             var entities = scope.GetEntities(query).ToArray();
             Assert.AreEqual(1, entities.Length);
             Assert.AreEqual(entityA, entities[0]);
             Assert.AreNotEqual(entityB, entities[0]);
         }
 
-        [Test]
-        public void RecoverObjectBitArray()
+        private static void QueryBeforeEntities<TEntity, TComponentType> (IScope<TEntity, TComponentType> scope, TComponentType type)
         {
-            var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new BitArrayStrategy(1));
+            var query = scope.CreateQuery(new[] { type }, Array.Empty<TComponentType>(), Array.Empty<TComponentType>());
+            var entityA = scope.CreateEntity();
+            var entityB = scope.CreateEntity();
+            scope.SetComponent(entityA, type, 100);
+            var entities = scope.GetEntities(query).ToArray();
+            Assert.AreEqual(1, entities.Length);
+            Assert.AreEqual(entityA, entities[0]);
+            Assert.AreNotEqual(entityB, entities[0]);
+        }
+
+        private static void RecoverObject<TEntity, TComponentType> (IScope<TEntity, TComponentType> scope, TComponentType type)
+        {
             var entity = scope.CreateEntity();
             var obj = new object();
-            scope.SetComponent(entity, 0, obj);
-            Assert.AreEqual(obj, scope.GetComponent<object>(entity, 0));
+            scope.SetComponent(entity, type, obj);
+            Assert.AreEqual(obj, scope.GetComponent<object>(entity, type));
         }
 
-        [Test]
-        public void RecoverObjectTypeHashSet()
+        private static void RecoverValueType<TEntity, TComponentType> (IScope<TEntity, TComponentType> scope, TComponentType type)
         {
-            var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
             var entity = scope.CreateEntity();
-            var obj = new object();
-            scope.SetComponent(entity, "a", obj);
-            Assert.AreEqual(obj, scope.GetComponent<object>(entity, "a"));
-        }
-
-        [Test]
-        public void RecoverValueTypeBitArray()
-        {
-            var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new BitArrayStrategy(1));
-            var entity = scope.CreateEntity();
-            scope.SetComponent(entity, 0, 42);
-            Assert.AreEqual(42, scope.GetComponent<int>(entity, 0));
-        }
-
-        [Test]
-        public void RecoverValueTypeTypeHashSet()
-        {
-            var entityId = 0;
-            var scope = Scope.CreateScope(() => entityId++, new TypeHashSetStrategy());
-            var entity = scope.CreateEntity();
-            scope.SetComponent(entity, "a", 42);
-            Assert.AreEqual(42, scope.GetComponent<int>(entity, "a"));
+            scope.SetComponent(entity, type, 42);
+            Assert.AreEqual(42, scope.GetComponent<int>(entity, type));
         }
     }
 }
