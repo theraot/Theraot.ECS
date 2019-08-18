135f113e17d89a06811e5ae8dabe4d00a17a0e0b
Alfonso J. Ramos <theraot@gmail.com>
2019-08-17 22:03:19 -0500

    New tests for component types

---
+++ Tests/ComponentTypeTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using Theraot.ECS;
+
+namespace Tests
+{
+    public class ComponentTypeTests
+    {
+        [Test]
+        public void ComponentTypeIsSetForAllEntities()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new SetManager());
+            var entityA = scope.CreateEntity();
+            var entityB = scope.CreateEntity();
+            scope.SetComponent(entityA, "test", 1);
+            Assert.Throws<ArgumentException>(() => scope.SetComponent(entityB, "test", "hello"));
+        }
+
+        [Test]
+        public void UnableToChangeComponentType()
+        {
+            var entityId = 0;
+            var scope = Scope.CreateScope(() => entityId++, new SetManager());
+            var entityA = scope.CreateEntity();
+            scope.SetComponent(entityA, "test", 1);
+            Assert.Throws<ArgumentException>(() => scope.SetComponent(entityA, "test", "hello"));
+        }
+    }
+}
