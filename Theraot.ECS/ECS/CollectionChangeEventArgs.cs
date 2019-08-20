16633c6110b5cba1975e9d3ad2600962a8711556
Alfonso J. Ramos <theraot@gmail.com>
2019-08-20 12:31:11 -0500

    EntityCollection events
    And yet another CollectionChangeEventArgs<T>
    
    Why is there not one in the BCL?

---
+++ Theraot.ECS/ECS/CollectionChangeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Theraot.ECS
+{
+    public class CollectionChangeEventArgs<T> : CollectionChangeEventArgs
+    {
+        public new T Element { get; }
+
+        public CollectionChangeEventArgs(CollectionChangeAction action, T element)
+            : base(action, element)
+        {
+            Element = element;
+        }
+    }
+}
--- Theraot.ECS/ECS/EntityCollection.cs
+++ Theraot.ECS/ECS/EntityCollection.cs
@@ -1,20 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Theraot.ECS
 {
     public sealed class EntityCollection<TEntity> : IEnumerable<TEntity>
     {
-        private readonly HashSet<TEntity> _hashSet;
+        private readonly HashSet<EventHandler<CollectionChangeEventArgs<TEntity>>> _addedEntity;
+        private readonly HashSet<EventHandler<CollectionChangeEventArgs<TEntity>>> _removedEntity;
+        private readonly HashSet<TEntity> _wrapped;
 
         internal EntityCollection()
         {
-            _hashSet = new HashSet<TEntity>();
+            _wrapped = new HashSet<TEntity>();
+            _removedEntity = new HashSet<EventHandler<CollectionChangeEventArgs<TEntity>>>();
+            _addedEntity = new HashSet<EventHandler<CollectionChangeEventArgs<TEntity>>>();
+        }
+
+        public event EventHandler<CollectionChangeEventArgs<TEntity>> AddedEntity
+        {
+            add => _addedEntity.Add(value);
+            remove => _addedEntity.Remove(value);
+        }
+
+        public event EventHandler<CollectionChangeEventArgs<TEntity>> RemovedEntity
+        {
+            add => _removedEntity.Add(value);
+            remove => _removedEntity.Remove(value);
         }
 
         public IEnumerator<TEntity> GetEnumerator()
         {
-            foreach (var entity in _hashSet)
+            foreach (var entity in _wrapped)
             {
                 yield return entity;
             }
@@ -27,12 +45,30 @@ namespace Theraot.ECS
 
         internal void Add(TEntity entity)
         {
-            _hashSet.Add(entity);
+            _wrapped.Add(entity);
+            OnAddedEntity(entity);
         }
 
         internal void Remove(TEntity entity)
         {
-            _hashSet.Remove(entity);
+            _wrapped.Remove(entity);
+            OnRemovedEntity(entity);
+        }
+
+        private void OnAddedEntity(TEntity entity)
+        {
+            foreach (var handler in _addedEntity)
+            {
+                handler.Invoke(this, new CollectionChangeEventArgs<TEntity>(CollectionChangeAction.Add, entity));
+            }
+        }
+
+        private void OnRemovedEntity(TEntity entity)
+        {
+            foreach (var handler in _removedEntity)
+            {
+                handler.Invoke(this, new CollectionChangeEventArgs<TEntity>(CollectionChangeAction.Remove, entity));
+            }
         }
     }
 }
