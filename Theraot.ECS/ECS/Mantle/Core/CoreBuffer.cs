60d485d8549ddab5198e3ade3298021425f12bd5
Alfonso J. Ramos <theraot@gmail.com>
2019-08-27 06:19:40 -0500

    Segregated CoreBuffer by ComponentType

--- Theraot.ECS/ECS/Mantle/Core/CoreBuffer.cs
+++ Theraot.ECS/ECS/Mantle/Core/CoreBuffer.cs
@@ -5,96 +5,113 @@ namespace Theraot.ECS.Mantle.Core
 {
     internal class CoreBuffer<TEntity, TComponentType>
     {
-        private List<KeyValuePair<EntityComponentsChangeEventArgs<TEntity, TComponentType>, object>> _log;
+        private Dictionary<TComponentType, HashSet<Operation>> _logDictionary;
 
         public bool BufferSetComponent<TComponent>(TEntity entity, TComponentType componentType, TComponent component)
         {
-            if (_log == null)
+            if (_logDictionary == null)
             {
                 return false;
             }
 
-            AddToLog(CollectionChangeActionEx.Add, entity, new[] { componentType }, new Func<TComponentType, TComponent>(_ => component));
+            AddToLog(CollectionChangeActionEx.Add, entity, componentType, component);
             return true;
         }
 
         public bool BufferSetComponents<TComponent>(TEntity entity, IList<TComponentType> componentTypes, Func<TComponentType, TComponent> componentSelector)
         {
-            if (_log == null)
+            if (_logDictionary == null)
             {
                 return false;
             }
 
-            AddToLog(CollectionChangeActionEx.Add, entity, componentTypes, componentSelector);
+            foreach (var componentType in componentTypes)
+            {
+                AddToLog(CollectionChangeActionEx.Add, entity, componentType, componentSelector(componentType));
+            }
             return true;
         }
 
         public bool BufferUnsetComponent(TEntity entity, IList<TComponentType> componentTypes)
         {
-            if (_log == null)
+            if (_logDictionary == null)
             {
                 return false;
             }
 
-            AddToLog(CollectionChangeActionEx.Remove, entity, componentTypes, null);
+            foreach (var componentType in componentTypes)
+            {
+                AddToLog(CollectionChangeActionEx.Remove, entity, componentType, null);
+            }
             return true;
         }
 
         public bool BufferUnsetComponent(TEntity entity, TComponentType componentType)
         {
-            if (_log == null)
+            if (_logDictionary == null)
             {
                 return false;
             }
 
-            AddToLog(CollectionChangeActionEx.Remove, entity, new[] { componentType }, null);
+            AddToLog(CollectionChangeActionEx.Remove, entity, componentType, null);
             return true;
         }
 
         public bool CreateBuffer()
         {
-            if (_log != null)
+            if (_logDictionary != null)
             {
                 return false;
             }
-            _log = new List<KeyValuePair<EntityComponentsChangeEventArgs<TEntity, TComponentType>, object>>();
+            _logDictionary = new Dictionary<TComponentType, HashSet<Operation>>();
             return true;
         }
 
         public void ExecuteBuffer<TComponentTypeSet>(ICore<TEntity, TComponentType, TComponentTypeSet> core)
         {
-            var log = _log;
-            _log = null;
-            foreach (var pair in log)
+            var logDictionary = _logDictionary;
+            _logDictionary = null;
+            foreach (var typeLogPair in logDictionary)
             {
-                var componentTypes = pair.Key.ComponentTypes;
-                var entity = pair.Key.Entity;
-                if (pair.Key.IsAdd)
+                foreach (var operation in typeLogPair.Value)
                 {
-                    core.SetComponents(entity, componentTypes, (Func<TComponentType, object>)pair.Value);
-                }
-                else
-                {
-                    core.UnsetComponents(entity, componentTypes);
+                    var componentType = typeLogPair.Key;
+                    if (operation.Action == CollectionChangeActionEx.Add)
+                    {
+                        core.SetComponent(operation.Entity, componentType, operation.Component);
+                    }
+                    else
+                    {
+                        core.UnsetComponent(operation.Entity, componentType);
+                    }
                 }
             }
         }
 
-        private void AddToLog(CollectionChangeActionEx action, TEntity entity, IList<TComponentType> componentTypes, object payload)
+        private void AddToLog(CollectionChangeActionEx action, TEntity entity, TComponentType componentType, object payload)
         {
-            _log.Add
-            (
-                new KeyValuePair<EntityComponentsChangeEventArgs<TEntity, TComponentType>, object>
-                (
-                    new EntityComponentsChangeEventArgs<TEntity, TComponentType>
-                    (
-                        action,
-                        entity,
-                        componentTypes
-                    ),
-                    payload
-                )
-            );
+            if (!_logDictionary.TryGetValue(componentType, out var log))
+            {
+                log = new HashSet<Operation>();
+                _logDictionary[componentType] = log;
+            }
+            log.Add(new Operation(entity, payload, action));
+        }
+
+        private sealed class Operation
+        {
+            public readonly CollectionChangeActionEx Action;
+
+            public readonly object Component;
+
+            public readonly TEntity Entity;
+
+            public Operation(TEntity entity, object component, CollectionChangeActionEx action)
+            {
+                Entity = entity;
+                Component = component;
+                Action = action;
+            }
         }
     }
 }
