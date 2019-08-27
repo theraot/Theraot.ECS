dbe52f48855e8dd4f63137a9f4c023034a6dfb96
Alfonso J. Ramos <theraot@gmail.com>
2019-08-27 08:00:55 -0500

    Fixing file naming

---
+++ Theraot.ECS/ECS/CollectionChangeActionEx.cs
@@ -0,0 +1,13 @@
+﻿namespace Theraot.ECS
+{
+    public enum CollectionChangeActionEx
+    {
+#if TARGETS_NET || GREATERTHAN_NETCOREAPP11
+        Add = System.ComponentModel.CollectionChangeAction.Add,
+        Remove = System.ComponentModel.CollectionChangeAction.Remove
+#else
+        Add = 1,
+        Remove = 2
+#endif
+    }
+}
