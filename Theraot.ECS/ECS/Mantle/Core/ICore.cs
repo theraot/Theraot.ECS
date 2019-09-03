d78604a9c6d0c2789d932d0194e4096e7d79f071
Alfonso J. Ramos <theraot@gmail.com>
2019-09-03 00:23:34 -0500

    Merged ICommon into ICore

--- Theraot.ECS/ECS/Mantle/Core/ICore.cs
+++ Theraot.ECS/ECS/Mantle/Core/ICore.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Theraot.ECS.Mantle.Core
 {
-    internal interface ICore<TEntity, TComponentType> : ICommon<TEntity, TComponentType>, IComponentReferenceAccessProvider<TEntity, TComponentType>
+    internal partial interface ICore<TEntity, TComponentType> : IComponentReferenceAccessProvider<TEntity, TComponentType>
     {
         event EventHandler<EntityComponentsChangeEventArgs<TEntity, TComponentType>> AddedComponents;
 
         event EventHandler<EntityComponentsChangeEventArgs<TEntity, TComponentType>> RemovedComponents;
 
+        Type GetRegisteredComponentType(TComponentType componentType);
+
         bool RegisterEntity(TEntity entity);
+
+        void SetComponent<TComponent>(TEntity entity, TComponentType type, TComponent component);
+
+        bool TryGetComponent<TComponent>(TEntity entity, TComponentType componentType, out TComponent component);
+
+        bool TryRegisterComponentType<TComponent>(TComponentType componentType);
+
+        void UnsetComponent(TEntity entity, TComponentType componentType);
+
+        void UnsetComponents(TEntity entity, IEnumerable<TComponentType> componentTypes);
     }
+
+#if LESSTHAN_NET35
+
+    internal partial interface ICore<TEntity, TComponentType>
+    {
+        void SetComponents<TComponent>(TEntity entity, IEnumerable<TComponentType> componentTypes, Converter<TComponentType, TComponent> componentSelector);
+    }
+
+#else
+
+    internal partial interface ICore<TEntity, TComponentType>
+    {
+        void SetComponents<TComponent>(TEntity entity, IEnumerable<TComponentType> componentTypes, Func<TComponentType, TComponent> componentSelector);
+    }
+
+#endif
 }
