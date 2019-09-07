4658056140fd31b1a3bd7ddfc2dc080fb61f4c23
Alfonso J. Ramos <theraot@gmail.com>
2019-09-06 22:38:41 -0500

    A few more tests

---
+++ Tests/MoreTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Theraot.ECS;
+
+namespace Tests
+{
+    public static class MoreTests
+    {
+        [Test]
+        public static void CreateScopeWithNullComponentKindManagerThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => Scope.CreateScope<int, int, int>(EqualityComparer<int>.Default, null));
+        }
+
+        [Test]
+        public static void DestroyExistingEntity()
+        {
+            var scope = Scope.CreateScope(EqualityComparer<int>.Default, new SetManager());
+            scope.RegisterEntity(0);
+            scope.DestroyEntity(0);
+        }
+
+        [Test]
+        public static void DestroyNonExistingEntity()
+        {
+            var scope = Scope.CreateScope(EqualityComparer<int>.Default, new SetManager());
+            Assert.Throws<KeyNotFoundException>(() => scope.DestroyEntity(0));
+        }
+    }
+}
